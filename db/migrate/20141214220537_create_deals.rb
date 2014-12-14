cc39c4ad4d7494d2b51ba027b6ce7352f3694ef0
Andrew <andrew.sunglae.kim@gmail.com>
2014-12-14 17:34:17 -0500

    migrations completed

--- db/migrate/20141214220537_create_deals.rb
+++ db/migrate/20141214220537_create_deals.rb
@@ -1,6 +1,6 @@
 class CreateDeals < ActiveRecord::Migration
   def change
-  	create_tables :deals do |t|
+  	create_table :deals do |t|
   		t.belongs_to :item
   		t.belongs_to :vendor
   		t.integer :price
