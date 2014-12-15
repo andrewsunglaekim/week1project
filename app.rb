832b6d5898491e3f3a8e89971129237afc9e2e75
Andrew <andrew.sunglae.kim@gmail.com>
2014-12-15 10:52:39 -0500

    added some views, added some get and post routes

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'bcrpyt'
+require 'pry'
 
 require_relative './models/user'
 require_relative './models/vendor'
 require_relative './models/item'
 require_relative './models/purchase'
 require_relative './models/deal'
-require 'pry'
 require_relative './config/environments'
+
 
 
 
@@ -15,6 +17,32 @@ require_relative './config/environments'
 # binding.pry
 
 get '/' do
+	@items = Item.all
 	
 	erb :index
 end
+
+get '/signup' do
+
+	erb :signup
+end
+
+post '/signup' do
+
+end
+
+get '/create_item' do
+
+	erb :create_item
+end
+
+post '/create_item' do
+	@item = Item.create(name: params[:name], description: params[:description])
+end
+
+get '/create_vendor' do
+
+	erb :create_vendor
+end
+
+
